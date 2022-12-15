349e1367b5bd4d09bc3fc473c9823a5b385a1aad
Steffen <99717310+ngutech21@users.noreply.github.com>
2022-12-15 17:57:52 +0100

    chore: extract DB-trait

--- src/db.rs
+++ src/db.rs
@@ -1,22 +1,32 @@
+use crate::model::Movie;
+use async_trait::async_trait;
 use sqlx::postgres::PgPoolOptions;
 
-use crate::model::Movie;
-
-pub struct DB {
+pub struct PostgresDB {
     pool: sqlx::Pool<sqlx::Postgres>,
 }
 
-impl DB {
-    pub async fn new() -> Result<DB, sqlx::Error> {
-        Ok(DB {
+impl PostgresDB {
+    pub async fn new() -> Result<PostgresDB, sqlx::Error> {
+        Ok(PostgresDB {
             pool: PgPoolOptions::new()
                 .max_connections(5)
                 .connect("postgres://axum:axum@localhost/axum_movies")
                 .await?,
         })
     }
+}
 
-    pub async fn query(&self, query: String) -> i64 {
+#[async_trait]
+pub trait DB {
+    async fn query(&self, query: String) -> i64;
+    async fn get_all_movies(&self) -> Vec<Movie>;
+    async fn import_movies(&self, movies: Vec<Movie>);
+}
+
+#[async_trait]
+impl DB for PostgresDB {
+    async fn query(&self, query: String) -> i64 {
         let row: (i64,) = sqlx::query_as("SELECT $1")
             .bind(150_i64)
             .fetch_one(&self.pool)
@@ -25,14 +35,14 @@ impl DB {
         row.0
     }
 
-    pub async fn get_all_movies(&self) -> Vec<Movie> {
+    async fn get_all_movies(&self) -> Vec<Movie> {
         let row = sqlx::query_as::<_, Movie>("SELECT * from movies").fetch_all(&self.pool);
         row.await.unwrap()
     }
 
-    pub async fn import_movies(&self, movies: Vec<Movie>) {
+    async fn import_movies(&self, movies: Vec<Movie>) {
         for movie in movies {
-            let result = sqlx::query(
+            sqlx::query(
                 r#"INSERT INTO public.movies (id, title) VALUES ($1, $2);
                 "#,
             )
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use axum::{extract::State, response::Html, routing::get, Json, Router};
-use axum_extra::response::ErasedJson;
+use db::PostgresDB;
+use db::DB;
 use model::Movie;
 use std::{net::SocketAddr, sync::Arc};
-use tracing::{event, span, Level};
+use tracing::{event, Level};
 
 use tower_http::trace::TraceLayer;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
@@ -10,9 +11,11 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 mod db;
 mod model;
 
+type DBState = State<Arc<Box<dyn DB + Send + Sync>>>;
+
 #[tokio::main]
 async fn main() {
-    let db = db::DB::new().await.unwrap();
+    let db = PostgresDB::new().await.unwrap();
     let result = db.get_all_movies().await;
 
     tracing_subscriber::registry()
@@ -22,31 +25,92 @@ async fn main() {
     let line = format!("movies {:?}", result);
     event!(Level::INFO, line);
 
-    let app = Router::new()
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app(Box::new(db)).into_make_service())
+        .await
+        .unwrap();
+}
+
+fn app(db: Box<dyn DB + Send + Sync>) -> Router {
+    Router::new()
         .route("/", get(handler))
         .route("/movies", get(get_movies))
         .route("/import_movies", get(import_movies))
         .with_state(Arc::new(db))
-        .layer(TraceLayer::new_for_http());
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("listening on {}", addr);
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+        .layer(TraceLayer::new_for_http())
 }
 
 async fn handler() -> Html<&'static str> {
     Html("<h1>Hello, World!</h1>")
 }
 
-async fn get_movies(State(db): State<Arc<db::DB>>) -> Json<Vec<Movie>> {
+async fn get_movies(State(db): DBState) -> Json<Vec<Movie>> {
     let result = db.get_all_movies().await;
     Json(result)
 }
 
-async fn import_movies(State(db): State<Arc<db::DB>>) -> Html<&'static str> {
+async fn import_movies(State(db): DBState) -> Html<&'static str> {
     let result = Movie::load_dummy_data();
     db.import_movies(result).await;
     Html("<h1>finished importing</h1>")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use async_trait::async_trait;
+    use axum::body::Body;
+    use axum::http::Request;
+    use axum::http::StatusCode;
+    use tower::ServiceExt; // for
+
+    struct MockDB {}
+
+    #[async_trait]
+    impl DB for MockDB {
+        async fn query(&self, _query: String) -> i64 {
+            0
+        }
+
+        async fn get_all_movies(&self) -> Vec<Movie> {
+            vec![Movie {
+                id: 666,
+                title: "foo".to_string(),
+            }]
+        }
+
+        #[allow(dead_code)]
+        async fn import_movies(&self, _movies: Vec<Movie>) {
+            unimplemented!()
+        }
+    }
+
+    fn create_mock_db() -> Box<dyn DB + Send + Sync> {
+        let db = MockDB {};
+        Box::new(db)
+    }
+
+    #[tokio::test]
+    async fn test_get_movies() {
+        let app = app(create_mock_db());
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .uri("/movies")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        let result_data: Vec<Movie> = serde_json::from_slice(&body).unwrap();
+        assert_eq!(result_data[0].id, 666);
+        assert_eq!(result_data[0].title, "foo");
+
+        dbg!(result_data);
+    }
 }
